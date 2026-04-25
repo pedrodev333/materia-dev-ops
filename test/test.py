7279335a80a872f0d41ef624585da29d30751bc5
Pedro <pedroheriquemendonca@gmail.com>
2026-04-25 19:07:43 -0300

    Corrigindo erros

--- test/test.py
+++ test/test.py
@@ -1,42 +1,49 @@
-from unittest import result
+import pytest
 
 from src.main import *
 from unittest.mock import patch
 
+
+@pytest.mark.asyncio
 def test_root():
     result = root()
     yield result
     assert result == {"message": "Hello World!"}
 
-
-def funcaoteste():
+@pytest.mark.asyncio
+def test_funcaoteste():
     with patch('random.randint', return_value=333653):
         result = funcaoteste()
         yield result
 
     assert result == {"teste": True, "num_aleatorio": 333653}
 
+@pytest.mark.asyncio
 def test_create_filme():
     filme_teste = Filme(id=1, name="Homem-Aranha 3", genre="Ação/Aventura", year=2007, rating=6.3)
     result = create_filme(filme_teste)
     yield result
     assert filme_teste == result
 
+@pytest.mark.asyncio
 def test_update_filme_negativo():
     result = update_filme(-5)
     yield result
     assert not result
 
+@pytest.mark.asyncio
 def test_update_filme_positivo():
     result = update_filme(10)
     yield result
     assert result
 
+@pytest.mark.asyncio
 def test_delete_filme_negativo():
     result = delete_filme(-5)
     yield result
     assert not result
 
+@pytest.mark.asyncio
 def test_delete_filme_positivo():
     result = delete_filme(10)
     yield result
