91dbaeaa1be11f7b01126cb9e38c2e5ab8afa0cf
Pedro <pedroheriquemendonca@gmail.com>
2026-04-25 18:52:51 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,29 +1,44 @@
+from unittest import result
+
 from src.main import *
 from unittest.mock import patch
 
 def test_root():
-    assert root() == {"message": "Hello World!"}
+    result = root()
+    yield result
+    assert result == {"message": "Hello World!"}
 
 
 def funcaoteste():
     with patch('random.randint', return_value=333653):
         result = funcaoteste()
+        yield result
 
     assert result == {"teste": True, "num_aleatorio": 333653}
 
 def test_create_filme():
     filme_teste = Filme(id=1, name="Homem-Aranha 3", genre="Ação/Aventura", year=2007, rating=6.3)
-    assert filme_teste == create_filme(filme_teste)
+    result = create_filme(filme_teste)
+    yield result
+    assert filme_teste == result
 
 def test_update_filme_negativo():
-    assert not update_filme(-5)
+    result = update_filme(-5)
+    yield result
+    assert not result
 
 def test_update_filme_positivo():
-    assert update_filme(10)
+    result = update_filme(10)
+    yield result
+    assert result
 
 def test_delete_filme_negativo():
-    assert update_filme(-5)
+    result = delete_filme(-5)
+    yield result
+    assert not result
 
 def test_delete_filme_positivo():
-    assert update_filme(10)
+    result = delete_filme(10)
+    yield result
+    assert result
 
