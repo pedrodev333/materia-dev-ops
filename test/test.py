626526ed4c2ae593961b5c38d7919d85662bb9ed
Pedro <pedroheriquemendonca@gmail.com>
2026-04-25 18:15:05 -0300

    Incluindo unidades de testes
    Incluindo unidades de testes no test/test.py

---
+++ test/test.py
@@ -0,0 +1,29 @@
+from src.main import *
+from unittest.mock import patch
+
+def test_root():
+    assert root() == {"message": "Hello World!"}
+
+def funcaoteste():
+    with patch('random.randint', return_value=333653):
+        result = funcaoteste()
+
+    assert result == {"teste": True, "num_aleatorio": 333653}
+
+def test_create_filme():
+    filme_teste = Filme(id=1, name="Homem-Aranha 3", genre="Ação/Aventura", year=2007, rating=6.3)
+    assert filme_teste == create_filme()
+
+def test_update_filme_negativo():
+    assert not update_filme(-5)
+
+def test_update_filme_positivo():
+    assert not update_filme(10)
+
+def test_delete_filme_negativo():
+    assert not delete_filme(-5)
+
+def test_delete_filme_positivo():
+    assert not delete_filme(10)
+
+
