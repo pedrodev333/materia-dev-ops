b89076e94eff762919a1570d97366fb0593871e9
Pedro <pedroheriquemendonca@gmail.com>
2026-04-25 19:11:51 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,40 +5,40 @@ from unittest.mock import patch
 
 
 @pytest.mark.asyncio
-def test_root():
-    result = root()
+async def test_root():
+    result = await root()
     assert result == {"message": "Hello World!"}
 
 @pytest.mark.asyncio
-def test_funcaoteste():
+async def test_funcaoteste():
     with patch('random.randint', return_value=333653):
-        result = funcaoteste()
+        result = await funcaoteste()
 
     assert result == {"teste": True, "num_aleatorio": 333653}
 
 @pytest.mark.asyncio
-def test_create_filme():
+async def test_create_filme():
     filme_teste = Filme(id=1, name="Homem-Aranha 3", genre="Ação/Aventura", year=2007, rating=6.3)
-    result = create_filme(filme_teste)
+    result = await create_filme(filme_teste)
     assert filme_teste == result
 
 @pytest.mark.asyncio
-def test_update_filme_negativo():
-    result = update_filme(-5)
+async def test_update_filme_negativo():
+    result = await update_filme(-5)
     assert not result
 
 @pytest.mark.asyncio
-def test_update_filme_positivo():
-    result = update_filme(10)
+async def test_update_filme_positivo():
+    result = await update_filme(10)
     assert result
 
 @pytest.mark.asyncio
-def test_delete_filme_negativo():
-    result = delete_filme(-5)
+async def test_delete_filme_negativo():
+    result = await delete_filme(-5)
     assert not result
 
 @pytest.mark.asyncio
-def test_delete_filme_positivo():
-    result = delete_filme(10)
+async def test_delete_filme_positivo():
+    result = await delete_filme(10)
     assert result
 
